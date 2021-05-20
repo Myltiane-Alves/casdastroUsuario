0cbcbcd2eab2cd73cef01008d028dbba54ddfec1
Myltiane Alves <myltiane.aux@gmail.com>
2021-05-20 08:38:23 -0300

    validação de formulários

--- controllers/UseControllers.js
+++ controllers/UseControllers.js
@@ -14,9 +14,13 @@ class UserController {
 
             event.preventDefault();
 
-            let values = this.getValues();
+            let btn = this.formEl.querySelector("[type=submit");
 
-            values.photo = "";
+            btn.disabled = true;
+
+            let values = this.getValues();
+            
+            if(!values) return false;
 
             this.getPhoto().then(
                 (content) => {
@@ -24,46 +28,48 @@ class UserController {
                     values.photo = content;
 
                     this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
                 },
                 (e) => {
-
                     console.error(e);
                 }
             );
 
-           this.addLine(this.getValues());
-
-        })
+        });
     }
 
     getPhoto() {
 
         return new Promise((resolve, reject) => {
 
-
-
             let fileReader = new FileReader();
     
-            let elements = [...this.formEl.elements].filter(item=> {
+            let elements = [...this.formEl.elements].filter(item => {
     
                 if(item.name === 'photo') {
                     return item;
                 }
-            })
+            });
     
             let file = elements[0].files[0];
     
             fileReader.onload = ()=>{
-    
-               
-                resolve(fileReader.result)
+            
+                resolve(fileReader.result);
             };
             
             fileReader.onerror = (e)=>{
                 reject(e);
             }
 
-            fileReader.readAsDataURL(file);
+            if(file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
         });
     }
 
@@ -71,19 +77,35 @@ class UserController {
 
         let user = {};
 
+        let isValid = true;
         [...this.formEl.elements].forEach(function(field, index){
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add('has-error')
+                isValid = false;
+                
+            }
 
             if(field.name === "gender") {
                 
                 if(field.checked) {
                     user[field.name] = field.value;
                 }
+            } else if(field.name == "admin") {
+
+                user[field.name] = field.checked;
+
             } else {
         
                 user[field.name] = field.value;
             }
-        })
-    
+        });
+        
+        if(!isValid) {
+            return false;
+        }
+
         return new User(
             user.name,
             user.gender,
@@ -101,22 +123,46 @@ class UserController {
     
     addLine(dataUser){
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td>
-                <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
-                </td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
+        let tr = document.createElement('tr');
+
+        tr.dataset.user = JSON.stringify(dataUser);
+
+        tr.innerHTML = `
+            
+            <td>
+            <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
+            </td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
+            <td>
+              <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
         
-            `; 
+        
+        `; 
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+
     }
 
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if(user._admin) numberAdmin++;
+        });
+
+        document.getElementById('number-users').innerHTML = numberUsers;
+        document.getElementById('number-users-admin').innerHTML = numberAdmin;
+    }
 }
